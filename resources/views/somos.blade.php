b010f9994d592fcd21d074c2daad762118f11bd1
unknown <santiagoaisi@gmail.com>
2019-12-07 12:12:10 -0300

    Agregando los modelos

---
+++ resources/views/somos.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="/css/app.css">
+    <title>somos</title>
+  </head>
+  <body>
+    <h1>Quienes somos</h1>
+    <h2> Nuestra Hitoria</h2>
+    <p>Somos una empresa familiar, nuevos en el mercado pero muy antiguos como fanáticos de toda la ciencia ficción y el mundo cómic</p>
+    <p>Nuestra historia comienza con la primer pelicula de Star Wars(La nueva esperanza)donde decimos pensar que habia mas gente alla afuera esperando tener en su casa un pedacito ese maravilloso universo, a partir de ahí comenzó toda esta aventura.</p>
+  </body>
+</html>
