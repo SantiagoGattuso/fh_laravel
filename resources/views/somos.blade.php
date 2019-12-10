51e5ef37c728656647cbd118451e7b24ac003edd
Tici <tittiangelucci08@gmail.com>
2019-12-10 15:11:20 -0300

    Modificacion de carpetas y somos

--- resources/views/somos.blade.php
+++ resources/views/somos.blade.php
@@ -1,14 +1,279 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="/css/app.css">
-    <title>somos</title>
-  </head>
-  <body>
-    <h1>Quienes somos</h1>
-    <h2> Nuestra Hitoria</h2>
-    <p>Somos una empresa familiar, nuevos en el mercado pero muy antiguos como fanáticos de toda la ciencia ficción y el mundo cómic</p>
-    <p>Nuestra historia comienza con la primer pelicula de Star Wars(La nueva esperanza)donde decimos pensar que habia mas gente alla afuera esperando tener en su casa un pedacito ese maravilloso universo, a partir de ahí comenzó toda esta aventura.</p>
-  </body>
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Star Freak</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Free HTML5 Website Template by gettemplates.co" />
+	<meta name="keywords" content="free website templates, free html5, free template, free bootstrap, free website template, html5, css3, mobile first, responsive" />
+	<meta name="author" content="gettemplates.co" />
+
+
+
+
+	<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+
+
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="{{asset('packages/css/animate.css')}}">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="{{asset('packages/icomoon.css')}}">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="{{asset('packages/css/bootstrap.css')}}">
+
+	<!-- Flexslider  -->
+	<link rel="stylesheet" href="{{asset('packages/css/flexslider.css')}}">
+
+	<!-- Owl Carousel  -->
+	<link rel="stylesheet" href="{{asset('packages/css/owl.carousel.min.css')}}">
+	<link rel="stylesheet" href="{{asset('packages/css/owl.theme.default.min.css')}}">
+
+	<!-- Theme style  -->
+	<link rel="stylesheet" href="{{asset('packages\css\style.css')}}">
+
+	<!-- Modernizr JS -->
+	<script src="{{asset('packages/js/modernizr-2.6.2.min.js')}}"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="{{asset('packages/js/respond.min.js')}}"></script>
+	<![endif]-->
+
+	</head>
+	<body>
+
+	<div class="fh5co-loader"></div>
+
+	<div id="page">
+	<nav class="fh5co-nav" role="navigation">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-3 col-xs-2">
+					<div id="fh5co-logo"><a href="index.html"><img src=packages/images/star1.jpg alt=""></a></div>
+				</div>
+				<div class="col-md-6 col-xs-6 text-center menu-1">
+					<ul>
+						<li class="has-dropdown">
+							<a href="product.html">Productos</a>
+							<ul class="dropdown">
+								<li><a href="single.html">universo Freak</a></li>
+							</ul>
+						</li>
+						<li><a href="about.html">Quienes Somos</a></li>
+						<li class="has-dropdown">
+						</li>
+						<li><a href="contact.html">Contacto</a></li>
+					</ul>
+				</div>
+				<div class="col-md-3 col-xs-4 text-right hidden-xs menu-2">
+					<ul>
+						<li class="search">
+							<div class="input-group">
+						      <input type="text" placeholder="Search..">
+						      <span class="input-group-btn">
+						        <button class="btn btn-primary" type="button"><i class="icon-search"></i></button>
+						      </span>
+						    </div>
+						</li>
+						<li class="shopping-cart"><a href="#" class="cart"><span><small>0</small><i class="icon-shopping-cart"></i></span></a></li>
+					</ul>
+				</div>
+			</div>
+
+		</div>
+	</nav>
+	<main>
+
+	<header id="fh5co-header" class="fh5co-cover fh5co-cover-sm" role="banner" style="background-image:url(packages/images/sw2.jpg);">
+		<div class="overlay"></div>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-8 col-md-offset-2 text-center">
+					<div class="display-t">
+						<div class="display-tc animate-box" data-animate-effect="fadeIn">
+							<h1>QUIENES SOMOS</h1>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</header>
+
+	<div id="fh5co-about">
+		<div class="container">
+			<div class="about-content">
+				<div class="row animate-box">
+					<div class="col-md-6">
+						<div class="desc">
+							<h3>Nuestra historia</h3>
+							<p>Somos una empresa familiar, nuevos en el mercado pero muy antiguos como fanáticos de toda la ciencia ficción y el mundo cómic</p>
+							<p>Nuestra historia comienza con la primer pelicula de Star Wars(La nueva esperanza)donde decimos pensar que habia mas gente alla afuera esperando tener en su casa un pedacito ese maravilloso universo, a partir de ahí comenzó toda esta aventura.</p>
+						</div>
+						<div class="desc">
+							<h3>Misión</h3>
+							<p>Nuestra única misión es poder llegar a todos los fanáticos que quieran un pedasito de estos universos paralelos que son los comic y la ciencia ficción</p>
+							<p>Desde nuestra posición como fanáticos vimos que era un mercada muy escaso para nuestras altas expectativas y por ello decimos cumplir esas expectativas</p>
+						</div>
+					</div>
+					<div class="col-md-6">
+						<img class="img-responsive" src="packages/images/sw7.jpg" alt="about">
+					</div>
+				</div>
+			</div>
+			<div class="row animate-box">
+				<div class="col-md-8 col-md-offset-2 text-center fh5co-heading">
+					<span>Resposables</span>
+					<h2>La Resistencia</h2>
+					<p>Con uds los resposables de esta linda locura</p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-4 col-sm-4 animate-box" data-animate-effect="fadeIn">
+					<div class="fh5co-staff">
+						<img src="packages/images/p1.jpg" alt="Free HTML5 Templates by gettemplates.co">
+						<h3>Santiago Gattuso</h3>
+						<strong class="role">General Eejecutivo</strong>
+						<p>Encargado de la parte admnistrativa y comercial con un unico sueño y objetivo llevar universos a las casas de los fanaticos.</p>
+						<ul class="fh5co-social-icons">
+							<li><a href="#"><i class="icon-facebook"></i></a></li>
+							<li><a href="#"><i class="icon-twitter"></i></a></li>
+							<li><a href="#"><i class="icon-instagram"></i></a></li>
+							<li><a href="#"><i class="icon-github"></i></a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-md-4 col-sm-4 animate-box" data-animate-effect="fadeIn">
+					<div class="fh5co-staff">
+						<img src="packages/images/p2.jpg" alt="Free HTML5 Templates by gettemplates.co">
+						<h3>Tici Angelucci</h3>
+						<strong class="role">Lider en tecnologias</strong>
+						<p>Fanatica N°1 de todo este universo, genia en tecnologia, es la persona culpable de que todos los fanaticos lleguen a este universo paralelo mediante esta pagina.</p>
+						<ul class="fh5co-social-icons">
+							<li><a href="#"><i class="icon-facebook"></i></a></li>
+							<li><a href="#"><i class="icon-twitter"></i></a></li>
+							<li><a href="#"><i class="icon-instagram"></i></a></li>
+							<li><a href="#"><i class="icon-github"></i></a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-md-4 col-sm-4 animate-box" data-animate-effect="fadeIn">
+					<div class="fh5co-staff">
+						<img src="packages/images/p3.jpg" alt="Free HTML5 Templates by gettemplates.co">
+						<h3>Javir verdejo</h3>
+						<strong class="role">Teniente Marketinero</strong>
+						<p>Encargado de difundir por todos los universos conocidos nuestros productos una persona clave para hacernos llegar a uds</p>
+						<ul class="fh5co-social-icons">
+							<li><a href="#"><i class="icon-facebook"></i></a></li>
+							<li><a href="#"><i class="icon-twitter"></i></a></li>
+							<li><a href="#"><i class="icon-instagram"></i></a></li>
+							<li><a href="#"><i class="icon-github"></i></a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div id="fh5co-started">
+		<div class="container">
+			<div class="row animate-box">
+				<div class="col-md-8 col-md-offset-2 text-center fh5co-heading">
+					<h2>Suscribete</h2>
+					<p>Suscribete para recibir promociones</p>
+				</div>
+			</div>
+			<div class="row animate-box">
+				<div class="col-md-8 col-md-offset-2">
+					<form class="form-inline">
+						<div class="col-md-6 col-sm-6">
+							<div class="form-group">
+								<label for="email" class="sr-only">Email</label>
+								<input type="email" class="form-control" id="email" placeholder="Email">
+							</div>
+						</div>
+						<div class="col-md-6 col-sm-6">
+							<button type="submit" class="btn btn-default btn-block">Subscribe</button>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<footer id="fh5co-footer" role="contentinfo">
+		<div class="container">
+			<div class="row row-pb-md">
+				<div class="col-md-4 fh5co-widget">
+					<h3>Star Freak</h3>
+					<p>Nos dedicamos a trasportar la imaginacion a la Realidad</p>
+				</div>
+				<div class="col-md-2 col-sm-4 col-xs-6 col-md-push-1">
+					<ul class="fh5co-footer-links">
+						<li><a href="#">Quienes Somos</a></li>
+					</ul>
+				</div>
+
+				<div class="col-md-2 col-sm-4 col-xs-6 col-md-push-1">
+					<ul class="fh5co-footer-links">
+						<li><a href="#">Productos</a></li>
+					</ul>
+				</div>
+				<div class="col-md-2 col-sm-4 col-xs-6 col-md-push-1">
+					<ul class="fh5co-footer-links">
+						<li><a href="#">Contacto</a></li>
+					</ul>
+				</div>
+			</div>
+
+			<div class="row copyright">
+				<div class="col-md-12 text-center">
+					<p>
+						<small class="block">&copy; 2019 Star Freak</small>
+							</p>
+					<p>
+						<ul class="fh5co-social-icons">
+							<li><a href="#"><i class="icon-twitter"></i></a></li>
+							<li><a href="#"><i class="icon-facebook"></i></a></li>
+							<li><a href="#"><i class="icon-linkedin"></i></a></li>
+							<li><a href="#"><i class="icon-instagram"></i></a></li>
+						</ul>
+					</p>
+				</div>
+			</div>
+
+		</div>
+	</footer>
+	</div>
+
+	<div class="gototop js-top">
+		<a href="#" class="js-gotop"><i class="icon-arrow-up"></i></a>
+	</div>
+
+	<!-- jQuery -->
+	<script src="{{asset('packages/js/jquery.min.js')}}"></script>
+	<!-- jQuery Easing -->
+	<script src="{{asset('packages/js/jquery.easing.1.3.js')}}"></script>
+	<!-- Bootstrap -->
+	<script src="{{asset('packages/js/bootstrap.min.js')}}"></script>
+	<!-- Waypoints -->
+	<script src="{{asset('packages/js/jquery.waypoints.min.js')}}"></script>
+	<!-- Carousel -->
+	<script src="{{asset('packages/js/owl.carousel.min.js')}}"></script>
+	<!-- countTo -->
+	<script src="{{asset('packages/js/jquery.countTo.js')}}"></script>
+	<!-- Flexslider -->
+	<script src="{{asset('packages/js/jquery.flexslider-min.js')}}"></script>
+
+	<!-- Main -->
+	<script src="{{asset('packages/js/main.js')}}"></script>
+
+	</body>
 </html>
